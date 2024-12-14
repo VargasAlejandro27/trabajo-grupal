ee4012181174886ea57e1c2ef7511541901de74a
luisvalarezo15 <luisvalarezo2007@gmail.com>
2024-12-14 13:36:13 -0500

    Ejercicio3.py

--- tjgrup.py
+++ tjgrup.py
@@ -1,11 +1,16 @@
-##Ejercicio 1
-numero = int(input("Ingrese su numero: "))
-rango = int(input("ingrese el rango de inicial: "))
-rango_fin = int(input("Ingrese rango limite: "))
-resultado = 1
-for i  in range (rango, rango_fin + 1 ,1):
-    if rango > 0:
-        resultado = numero * i
-        print(f"{numero} x {i} = {resultado}")
-    else:
-        print("no se puede hacerlo con nuermoes negativos")    
+
+x = 0
+y = 1
+z = 0
+num =int(input("Ingrese numero maximo :"))
+print(x)
+print(y)
+
+for i in range(2,num,1) :
+    z = x + y
+    print(z)
+    x = y
+    y = z
+    
+    suma =  y + z + x - 1
+print ("La suma de los resultado es  :",suma)
