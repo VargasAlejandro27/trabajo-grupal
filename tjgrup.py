0d218ab5d8df3a9464a9b79f79fde7813e900e53
VargasAlejandro27 <avvargas@puce.edu.ec>
2024-12-14 13:49:28 -0500

    Update tjgrup.py

--- tjgrup.py
+++ tjgrup.py
@@ -10,26 +10,3 @@ for i  in range (rango, rango_fin + 1 ,1):
     else:
         print("no se puede hacerlo con nuermoes negativos")    
 
-
-
-
-
-
-
-##Ejercicio :3 fivonacci
-
-x = 0
-y = 1
-z = 0
-num =int(input("Ingrese numero maximo :"))
-print(x)
-print(y)
-
-for i in range(2,num,1) :
-    z = x + y
-    print(z)
-    x = y
-    y = z
-    
-    suma =  y + z + x - 1
-print ("La suma de los resultado es  :",suma)
